5eb8cb25814eb3f800e305d44c825152534cc889
Conor Vedova <conor.vedova@mail.utoronto.ca>
2020-12-18 11:40:39 -0500

    Finished DLT code

--- direct_linear_transform.py
+++ direct_linear_transform.py
@@ -1,4 +1,5 @@
 import numpy as np
+import funcs
 
 def normalize_and_make_homogeneous(x_unnormalized):
     """Modify x_unnormalized to normalize the vector according to standard DLT methods and make homogeneous. Normalization is used to stabilize calculation of DLT
@@ -17,11 +18,10 @@ def normalize_and_make_homogeneous(x_unnormalized):
     x_unnormalized = np.dot(transform, np.concatenate((x_unnormalized.T, np.ones((1,x_unnormalized.shape[0])))))
     x_unnormalized = x_unnormalized[0:x_unnormalized.shape[1], :].T
 
-    return transform, x
+    return transform, x_unnormalized
 
 def calculate_calibration_matrix(points3D, points2D):
-    """Use DLT to calculate the 11 params of the calibration matrix. Calibration matrix transforms from 3D to 2D.
-    """
+    """Use DLT to calculate the 11 params of the calibration matrix. Calibration matrix transforms from 3D to 2D."""
     transform3D, points3D_norm = normalize_and_make_homogeneous(points3D)
     transform2D, points2D_norm = normalize_and_make_homogeneous(points2D)
     
@@ -39,65 +39,18 @@ def calculate_calibration_matrix(points3D, points2D):
     calibration_matrix = np.dot(np.linalg.pinv(transform2D), np.dot(H, transform3D))
     return calibration_matrix
     
-def get_calibration_points(old_points, old_points3D, new_image):
+def get_calibration_points(old_points, old_points3D, new_image, dist):
     """Match feature points for the new image to the point cloud."""
+    _, mask_a, points2D, _ = funcs.find_matches(old_points[:,0], old_points[:,1], new_image[:,0], new_image[:,1], dist)
+    points_3D = old_points3D[mask_a]
+    return points3D, points2D
     
-    
-def perform_dlt(old_points, old_points3D, new_image):
+def perform_dlt(old_points, old_points3D, new_image, dist=0.7):
     """Perform dlt on the new image to get camera matrix.
     
-    old_points: points and """
-    
-    
+    old_points: 2D points in pointcloud from image 1 or 2. numpy array with old_points[:,0] being 2d points and old_points[:,1] being description vectors for the points
+    old_points3D: same points as old_points but with depth. old_points3D are in the same order as old_points
+    new_image: 2D points from image 3. numpy array with new_image[:,0] being 2d points and new_image[:,1] being description vectors for the points
+    """
+    points3D, points2D = get_calibration_points(old_points, old_points3D, new_image, dist)
     return calculate_calibration_matrix(points3D, points2D)
-
-#Filter matches for image 3
-if np.sum(mask_23) > np.sum(mask_13):
-    E, mask = cv.findEssentialMat(matches_23, matches_32)
-
-    # Filter the matches further down
-    filtered_3_forward = matches_23[(mask == 1)[:,0]]
-    filtered_3_backward = matches_32[(mask == 1)[:,0]]
-    P = P2
-    largest_sum =  np.sum(mask_23)
-    current_img = img_2
-else:
-    E, mask = cv.findEssentialMat(matches_13, matches_31)
-
-    # Filter the matches further down
-    filtered_3_forward = matches_13[(mask == 1)[:,0]]
-    filtered_3_backward = matches_31[(mask == 1)[:,0]]
-    P = P1
-    largest_sum =  np.sum(mask_13)
-    current_image = img_1
-
-print(largest_sum)
-print(len(filtered_3_forward))
-print(len(filtered_3_backward))
-if len(filtered_3_forward) < 200:
-    #Create new keyframe and triangulate
-    ret, R, t, _ = cv.recoverPose(E, filtered_3_forward, filtered_3_backward)
-    # Create the minimization function
-    min_func = funcs.make_min_function(P, filtered_3_forward, filtered_3_backward, K, R, t)
-
-    # Now it gets fun
-    # Miniminize the function to get an estimate of lambda (for the translation)
-    # Use 'Powell' (might need more testing)
-    res = scipy.optimize.minimize(min_func, 1, method='Powell')
-
-    # Create the second camera
-    P3 = funcs.camera_compose(K, R, t * res.x[0])
-    
-    # Go through the matches
-    for point_a, point_b in zip(filtered_3_forward, filtered_3_backward):
-        # Triangulate the points
-        vec = funcs.triangulate(point_a, point_b, P, P3)
-
-        # Get the pixel
-        pixel = current_img[int(point_a[1])][int(point_a[0])]
-
-        # Add it to the cloud
-        cloud.append([vec[0], vec[1], vec[2], pixel[2], pixel[1], pixel[0]])
-else:
-    #Apply DLT
-    pass
